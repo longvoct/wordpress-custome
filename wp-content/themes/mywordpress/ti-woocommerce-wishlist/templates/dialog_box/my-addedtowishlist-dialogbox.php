8eda07b99afcecba1a69ae71f3b7e78a8c3074ef
Long Võ <volonglqd@gmail.com>
2023-04-28 18:41:16 +0700

    Update Code

---
+++ wp-content/themes/mywordpress/ti-woocommerce-wishlist/templates/dialog_box/my-addedtowishlist-dialogbox.php
@@ -0,0 +1,4 @@
+<div class="ti-wishlist-added-message">
+  <p>Đã thêm sản phẩm vào danh sách yêu thích của bạn.</p>
+  <a href="<?php echo esc_url($wishlist_url); ?>">Xem danh sách yêu thích</a>
+</div>
