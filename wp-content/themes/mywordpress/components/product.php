6b2eb0299886b25e643a9ae4744556d1aec39787
Vo Long <volonglqd@gmail.com>
2023-05-25 12:35:15 +0700

    Update code

--- wp-content/themes/mywordpress/components/product.php
+++ wp-content/themes/mywordpress/components/product.php
@@ -1,15 +1,36 @@
 <?php
 $product_id = get_the_ID();
 $product = wc_get_product($product_id);
+$variations = $product->get_available_variations();
 ?>
 
 <div class="product-list__item">
-  <a href="<?php the_permalink(); ?>">
-    <?php echo get_the_post_thumbnail(get_the_ID(), 'thumbnail', array('class' => 'product-list__image')); ?>
-  </a>
+  <div>
+    <a href="<?php the_permalink(); ?>">
+      <?php echo get_the_post_thumbnail(get_the_ID(), 'thumbnail', array('class' => 'product-list__image')); ?>
+    </a>
+  </div>
   <h3 class="product-list__name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
   <p class="product-list__price"><?php echo $product->get_price_html(); ?></p>
   <span class="my-favorite">
     <?php echo do_shortcode('[ti_wishlists_addtowishlist]'); ?>
   </span>
+
+  <div class="product-list__variations">
+    <?php
+    $colors = array(); // Khởi tạo mảng để lưu các màu sắc duy nhất
+    foreach ($variations as $variation) :
+      // Kiểm tra nếu biến thể có thuộc tính màu sắc
+      if (isset($variation['attributes']['attribute_pa_color'])) {
+        $color = $variation['attributes']['attribute_pa_color'];
+        // Nếu màu sắc chưa tồn tại trong mảng, thêm vào mảng
+        if (!in_array($color, $colors)) {
+          $colors[] = $color;
+        }
+      }
+    endforeach;
+    // Lấy số lượng màu sắc có sẵn
+    $color_count = count($colors);
+    ?> <p>Số màu có sẵn: <?php echo $color_count; ?></p>
+  </div>
 </div>
--- wp-content/themes/mywordpress/functions.php
+++ wp-content/themes/mywordpress/functions.php
@@ -224,36 +224,3 @@ function my_pagination_setup()
   add_theme_support('paginate_links');
 }
 add_action('after_setup_theme', 'my_pagination_setup');
-
-// Tùy chỉnh ảnh gallery bị mờ
-// add_filter('woocommerce_get_image_size_gallery_thumbnail', function ($size) {
-//   return array(
-//     'width' => 150,
-//     'height' => 150,
-//     'crop' => 0,
-//   );
-// });
-
-// add_filter('single_product_archive_thumbnail_size', function ($size) {
-//   return array(
-//     'width' => 500,
-//     'height' => 500,
-//     'crop' => 0,
-//   );
-// });
-
-// add_filter('subcategory_archive_thumbnail_size', function ($size) {
-//   return array(
-//     'width' => 500,
-//     'height' => 500,
-//     'crop' => 0,
-//   );
-// });
-
-// add_filter('woocommerce_gallery_thumbnail_size', function ($size) {
-//   return array(
-//     'width' => 500,
-//     'height' => 500,
-//     'crop' => 0,
-//   );
-// });
