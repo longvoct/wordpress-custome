27ed541f5fff9ed666456531dab473c216274464
Vo Long <volonglqd@gmail.com>
2023-05-27 11:07:21 +0700

    Update

--- wp-content/themes/mywordpress/footer.php
+++ wp-content/themes/mywordpress/footer.php
@@ -1,37 +1,74 @@
 <div class="footer">
   <div class="footer-top">
-    <ul style="display: flex;
-    justify-content: space-between;
-    align-items: flex-start;">
-      <li class="footer_title">
-        <span>VỀ ORDERSHOES</span>
-        <ul>
-          <li>Giới thiệu</li>
-          <li>Ưu đãi</li>
-          <li>Tin tức</li>
-          <li>Tuyển dụng</li>
-        </ul>
-      </li>
-      <li class="footer_title">
-        <span>HỖ TRỢ KHÁCH HÀNG</span>
-        <ul>
-          <li>Hướng dẫn chọn size</li>
-          <li>Chính sách bảo mật</li>
-          <li>Thanh toán - giao nhận</li>
-          <li>Chính sách khách hàng</li>
-        </ul>
-      </li>
-      <li class="footer_title">
-        <span>THÔNG TIN LIÊN HỆ</span>
-        <ul>
-          <li>Liên hệ đặt hàng: 0972.101.281</li>
-          <li>Địa chỉ: Linh Trung, Thủ Đức</li>
-          <li>Email: ordershoescv@gmail.com</li>
-        </ul>
-      </li>
-    </ul>
+    <div class="footer-top__left">
+      <div class="logo">
+        <a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/logo-white.png"
+            alt="<?php bloginfo('name'); ?>" class="img-logo" /></a>
+      </div>
+      <p style="margin-top: 10px; font-weight: 500;">Nhận các thông báo mới của ORDERSHOE</p>
+      <div class="field-email" style="margin-top: 15px; display: flex;
+      justify-content: space-between;
+      align-items: center;">
+        <input type="text" placeholder="Nhập địa chỉ email" class="footer-email_input" />
+        <div class="footer-top">
+          <button>Đăng ký</button>
+        </div>
+      </div>
+    </div>
+    <div class="footer-top__right">
+      <ul style="display: flex;
+      justify-content: space-between;
+      align-items: flex-start;">
+        <li class="footer_title">
+          <span>VỀ ORDERSHOES</span>
+          <ul>
+            <li>Giới thiệu</li>
+            <li>Ưu đãi</li>
+            <li>Tin tức</li>
+            <li>Tuyển dụng</li>
+          </ul>
+        </li>
+        <li class="footer_title">
+          <span>DANH MỤC SẢN PHẨM</span>
+          <ul>
+            <li>Giày cho nam</li>
+            <li>Giày cho nữ</li>
+            <li>Trẻ em</li>
+          </ul>
+        </li>
+        <li class="footer_title">
+          <span>HỖ TRỢ KHÁCH HÀNG</span>
+          <ul>
+            <li>Hướng dẫn chọn size</li>
+            <li>Chính sách bảo mật</li>
+            <li>Thanh toán - giao nhận</li>
+            <li>Chính sách khách hàng</li>
+          </ul>
+        </li>
+        <li class="footer_title">
+          <span>THÔNG TIN LIÊN HỆ</span>
+          <ul>
+            <li>Liên hệ đặt hàng: 0972.101.281</li>
+            <li>Địa chỉ: Linh Trung, Thủ Đức</li>
+            <li>Email: ordershoescv@gmail.com</li>
+            <li class="footer-icon__list">
+              <img src="<?php bloginfo('template_directory'); ?>/images/facebook.png" alt=""
+                class="footer-icon__social" />
+              <img src="<?php bloginfo('template_directory'); ?>/images/instagram.png" alt=""
+                class="footer-icon__social" />
+              <img src="<?php bloginfo('template_directory'); ?>/images/tik-tok.png" alt=""
+                class="footer-icon__social" />
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
-  <div class="footer=bottom"></div>
+  <div class="footer-bottom">
+    <div class="footer-line"></div>
+    <p style="margin-top: 15px; font-size:13px; color: #ccc;">@2023 - Copyright belongs to ORDERSHOES</p>
+  </div>
+
 </div>
 <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 <?php wp_footer(); ?>
