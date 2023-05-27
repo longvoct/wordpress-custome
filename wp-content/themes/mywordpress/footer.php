50007394da9ec1da786824bf1146f032d52cda59
Vo Long <volonglqd@gmail.com>
2023-05-27 09:39:55 +0700

    Update Code

--- wp-content/themes/mywordpress/footer.php
+++ wp-content/themes/mywordpress/footer.php
@@ -1,23 +1,37 @@
 <div class="footer">
   <div class="footer-top">
-    <span>Lorem ipsum dolor sit ame</span>
-    <button>Button</button>
+    <ul style="display: flex;
+    justify-content: space-between;
+    align-items: flex-start;">
+      <li class="footer_title">
+        <span>VỀ ORDERSHOES</span>
+        <ul>
+          <li>Giới thiệu</li>
+          <li>Ưu đãi</li>
+          <li>Tin tức</li>
+          <li>Tuyển dụng</li>
+        </ul>
+      </li>
+      <li class="footer_title">
+        <span>HỖ TRỢ KHÁCH HÀNG</span>
+        <ul>
+          <li>Hướng dẫn chọn size</li>
+          <li>Chính sách bảo mật</li>
+          <li>Thanh toán - giao nhận</li>
+          <li>Chính sách khách hàng</li>
+        </ul>
+      </li>
+      <li class="footer_title">
+        <span>THÔNG TIN LIÊN HỆ</span>
+        <ul>
+          <li>Liên hệ đặt hàng: 0972.101.281</li>
+          <li>Địa chỉ: Linh Trung, Thủ Đức</li>
+          <li>Email: ordershoescv@gmail.com</li>
+        </ul>
+      </li>
+    </ul>
   </div>
-  <div class="footer-line"></div>
-  <div class="footer-bottom">
-    <div class="footer-bottom__left">
-      <img src="<?php bloginfo('template_directory'); ?>/images/logo-white.png" alt="" class="img-logo" />
-      <span>Lorem ipsum dolor sit amet consectetur</span>
-      <span>Arcu dolor tellus sapien diam nisi id.</span>
-      <span>Lorem ipsum dolor sit amet consectetur.</span>
-    </div>
-    <div class="footer-bottom__right">
-      <img src="<?php bloginfo('template_directory'); ?>/images/facebook.png" alt="" class="footer-icon__social" />
-      <img src="<?php bloginfo('template_directory'); ?>/images/instagram.png" alt="" class="footer-icon__social" />
-      <img src="<?php bloginfo('template_directory'); ?>/images/tik-tok.png" alt="" class="footer-icon__social" />
-    </div>
-  </div>
-  <span>©2023 abcxyz. All rights reversed</span>
+  <div class="footer=bottom"></div>
 </div>
 <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 <?php wp_footer(); ?>
