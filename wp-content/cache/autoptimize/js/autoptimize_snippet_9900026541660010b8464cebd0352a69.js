4804ee6af9eb15d041bc42ac38cd21d499692898
Vo Long <volonglqd@gmail.com>
2023-06-08 17:20:34 +0700

    Update code

--- wp-content/cache/autoptimize/js/autoptimize_snippet_9900026541660010b8464cebd0352a69.js
+++
@@ -1,1 +0,0 @@
-var filterTitles=document.querySelectorAll(".filter-title");filterTitles.forEach((filterTitle)=>{filterTitle.addEventListener("click",function(){console.log("click me");var filterContent=this.nextElementSibling;filterTitle.classList.toggle("collapsed");filterContent.classList.toggle("active");});});
